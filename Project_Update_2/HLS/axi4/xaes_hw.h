de3dd1fea29273c4dde02ce36ec78953c1567e19
Zanatticus <zingare@live.com>
2024-03-20 16:12:15 -0400

    massive reorg and new axi4 dir

--- Project_Update_2/HLS/axi4/xaes_hw.h
+++ Project_Update_2/HLS/axi4/xaes_hw.h
@@ -4,10 +4,25 @@
 // Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 // ==============================================================
 // control
-// 0x00 : reserved
-// 0x04 : reserved
-// 0x08 : reserved
-// 0x0c : reserved
+// 0x00 : Control signals
+//        bit 0  - ap_start (Read/Write/COH)
+//        bit 1  - ap_done (Read/COR)
+//        bit 2  - ap_idle (Read)
+//        bit 3  - ap_ready (Read/COR)
+//        bit 7  - auto_restart (Read/Write)
+//        bit 9  - interrupt (Read)
+//        others - reserved
+// 0x04 : Global Interrupt Enable Register
+//        bit 0  - Global Interrupt Enable (Read/Write)
+//        others - reserved
+// 0x08 : IP Interrupt Enable Register (Read/Write)
+//        bit 0 - enable ap_done interrupt (Read/Write)
+//        bit 1 - enable ap_ready interrupt (Read/Write)
+//        others - reserved
+// 0x0c : IP Interrupt Status Register (Read/COR)
+//        bit 0 - ap_done (Read/COR)
+//        bit 1 - ap_ready (Read/COR)
+//        others - reserved
 // 0x10 : Data signal of plaintext
 //        bit 31~0 - plaintext[31:0] (Read/Write)
 // 0x14 : Data signal of plaintext
@@ -33,6 +48,10 @@
 // 0x44 : reserved
 // (SC = Self Clear, COR = Clear on Read, TOW = Toggle on Write, COH = Clear on Handshake)
 
+#define XAES_CONTROL_ADDR_AP_CTRL            0x00
+#define XAES_CONTROL_ADDR_GIE                0x04
+#define XAES_CONTROL_ADDR_IER                0x08
+#define XAES_CONTROL_ADDR_ISR                0x0c
 #define XAES_CONTROL_ADDR_PLAINTEXT_DATA     0x10
 #define XAES_CONTROL_BITS_PLAINTEXT_DATA     64
 #define XAES_CONTROL_ADDR_CIPHERTEXT_DATA    0x1c
@@ -43,4 +62,3 @@
 #define XAES_CONTROL_BITS_KEY_SIZE_DATA      32
 #define XAES_CONTROL_ADDR_DECRYPTEDTEXT_DATA 0x3c
 #define XAES_CONTROL_BITS_DECRYPTEDTEXT_DATA 64
-
