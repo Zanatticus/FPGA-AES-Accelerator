b4e1ff933633263ad1762dfb8d4a491e50286119
Zanatticus <zingare@live.com>
2024-03-23 22:47:52 -0400

    compress header file

--- Project_Update_2/HLS/axi_stream/vitis_source/aes_axis.h
+++ Project_Update_2/HLS/axi_stream/vitis_source/aes_axis.h
@@ -13,27 +13,20 @@ enum errorCode
     ERROR_AES_UNKNOWN_KEYSIZE,
     ERROR_MEMORY_ALLOCATION_FAILED,
 };
-
-unsigned char getSBoxValue(unsigned char num);
-unsigned char getSBoxInvert(unsigned char num);
-
-void rotate(unsigned char *word);
-
-unsigned char getRconValue(unsigned char num);
-
-// Key Schedule Core
-void core(unsigned char *word, int iteration);
-
 enum keySize
 {
     SIZE_16 = 16,
     SIZE_24 = 24,
     SIZE_32 = 32
 };
-
+unsigned char getSBoxValue(unsigned char num);
+unsigned char getSBoxInvert(unsigned char num);
+void rotate(unsigned char *word);
+unsigned char getRconValue(unsigned char num);
+// Key Schedule Core
+void core(unsigned char *word, int iteration);
 // Key Expansion
 void expandKey(unsigned char *expandedKey, unsigned char *key, enum keySize, size_t expandedKeySize);
-
 // AES Encryption
 void subBytes(unsigned char *state);
 void shiftRows(unsigned char *state);
